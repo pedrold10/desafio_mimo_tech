5f866a0b027b2cad501a3a02a4937f317cab44d9
Pedro Gabriel <58262161+pedrold10@users.noreply.github.com>
2023-05-29 15:39:06 -0300

    Recomeçando projeto

--- tools-api/src/database/services/index.ts
+++
@@ -1,8 +0,0 @@
-import createDynamoDBClient from "../db";
-import ToolService from "./toolService";
-
-const { TOOLS_TABLE } = process.env;
-
-const toolService = new ToolService(createDynamoDBClient(), TOOLS_TABLE);
-
-export default toolService;
